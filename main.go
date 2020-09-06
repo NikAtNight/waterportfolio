3acfc0dcfd4b1298afec90d289750e94a0532eb1
Nikhil <nikhil.kapadia.97@gmail.com>
2020-09-06 13:00:50 -0400

    Updated main to handle spa correctly using gorilla/mux

--- main.go
+++ main.go
@@ -4,12 +4,49 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 )
+
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	path = filepath.Join(h.staticPath, r.URL.Path)
+
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
+}
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	//Serve Other Routes Above PathPrefix
+	spa := spaHandler{staticPath: "./homepage/build", indexPath: "index.html"}
+	router.PathPrefix("/").Handler(spa)
 
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./homepage/build")))
-	log.Fatal(http.ListenAndServe(":8080", router))
+	svr := &http.Server{
+		Handler:      router,
+		Addr:         ":8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(svr.ListenAndServe())
 }
